96622eca1393e296348de893b68a90e9a5f5266c
sphinx-austin <austinjuniour4@gmail.com>
2023-09-06 19:39:23 +0300

    update

--- registration/views.py
+++ registration/views.py
@@ -1,10 +1,55 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from .forms import CreateUserForm
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
+from django.forms import inlineformset_factory
 
 # Create your views here.
-from django.shortcuts import render
 
-def register(request):
-    return render(request, 'registration/register.html')
 
-def login(request):
-    return render(request, 'registration/login.html')
+# REGISTER
+def registerUser(request):
+    if request.user.is_authenticated:
+        return redirect('home')
+    else:
+        form = CreateUserForm ()
+
+        if request.method == 'POST':
+            form = CreateUserForm (request.POST)
+            if form.is_valid():
+                form.save()
+                user = form.cleaned_data.get('username')
+                success_message = messages.success(request, 'Account successfully created for '+ user)
+                return redirect('login')
+
+        context = {'form':form}
+        return render(request, 'registration/register.html', context)
+
+
+# LOGIN
+def loginUser(request):
+    if request.user.is_authenticated:
+        return redirect('home')
+    else:
+        if request.method == 'POST':
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+
+            user = authenticate(request, username=username, password=password)
+
+            if user is not None:
+                login(request, user)
+                return redirect('home')
+            else:
+                messages.info(request, 'Username or Password is incorrect!')
+
+        return render(request, 'registration/login.html')
+
+# LOGOUT
+def logoutUser(request):
+    logout(request)
+    return redirect('login')
