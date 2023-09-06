eb34634854dfa48633481c9cfb03eca2cea0bb0c
sphinx-austin <austinjuniour4@gmail.com>
2023-09-06 16:49:03 +0300

    update

--- registration/views.py
+++ registration/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render
+
+def register(request):
+    return render(request, 'registration/register.html')
+
+def login(request):
+    return render(request, 'registration/login.html')
