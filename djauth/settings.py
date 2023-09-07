a6474c32c41dd975f1d23eb8d6d6d2e5062f81a4
sphinx-austin <austinjuniour4@gmail.com>
2023-09-07 13:36:14 +0300

    update

--- djauth/settings.py
+++ djauth/settings.py
@@ -44,6 +44,10 @@ INSTALLED_APPS = [
     'compressor',
 ]
 
+# custom user
+AUTH_USER_MODEL = 'registration.User'
+
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -121,6 +125,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+MEDIA_URL = '/images/'
+
+MEDIA_ROOT = BASE_DIR / 'profile/images'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
